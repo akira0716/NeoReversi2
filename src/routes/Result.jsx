af7daf8806fd7410e3eaadd635ccde825569a3ef
Nao <70929nao@gmail.com>
2023-11-14 00:36:48 +0900

    result更新
    内容変更

--- src/routes/Result.jsx
+++ src/routes/Result.jsx
@@ -1,9 +1,25 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
+import "../css/result.css";
 
 const Result = () => {
   return (
     <div>
       <h1>結果画面</h1>
+
+      <div
+        className="sample
+"
+      ></div>
+      <div className="btn-wrap">
+        <Link to="/PlayGame" className="btn2 onemore">
+          もっかい遊べるドン
+        </Link>
+        <Link to="/" className="btn2 end">
+          やめたいんご～
+        </Link>
+      </div>
     </div>
   );
 };
