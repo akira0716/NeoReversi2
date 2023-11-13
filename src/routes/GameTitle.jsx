563adc87a8def04fcb37fed94f4802764dc8b704
[Mzty32] <[blue.train.tm@gmail.com]>
2023-11-14 00:12:23 +0900

    タイトル画面_デザインの作成

--- src/routes/GameTitle.jsx
+++ src/routes/GameTitle.jsx
@@ -1,22 +1,48 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import MusoImg from "../public/muso.png";
 
 const Home = () => {
   return (
-    <div>
-      <h1>トップページ</h1>
+    <div className="flex flex-col items-center justify-center h-screen">
+      <img src={MusoImg} alt="NEO Reversi ～無双～ 2" />
       <br />
-      <p>
-        <Link to="/PlayGame">
-          <button className="btn btn-primary">Game Start</button>
-        </Link>
-      </p>
+      <p className="text-center">プレイ人数</p>
       <br />
-      <p>
-        <Link to="/Result">
-          <button className="btn btn-primary">結果画面</button>
-        </Link>
-      </p>
+      <div className="flex space-x-4">
+        <button className="btn btn-outline btn-success">2人</button>
+        <button className="btn btn-outline btn-success">3人</button>
+        <button className="btn btn-outline btn-success">4人</button>
+        <button className="btn btn-outline btn-success">5人</button>
+      </div>
+
+      <br />
+      <p>モード</p>
+      <br />
+      <div className="flex space-x-4">
+        <br />
+        <button className="btn btn-outline btn-info">いつもの</button>
+        <button className="btn btn-outline btn-error">無双</button>
+      </div>
+
+      <br />
+      <div className="flex">
+        <p>
+          <Link to="/PlayGame">
+            <button className="btn btn-outline btn-primary">
+              Are you ready？？
+            </button>
+          </Link>
+        </p>
+      </div>
+      <br />
+      <div className="flex">
+        <p>
+          <Link to="/Result">
+            <button className="btn btn-outline btn-warning">結果画面</button>
+          </Link>
+        </p>
+      </div>
     </div>
   );
 };
