9c5c3e7bcdfe33b5befd2ed9a42e227a818522a0
akira0716 <akira.inaji.0716@gmail.com>
2023-11-14 22:44:29 +0900

    オセロ完成では？

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,63 +1,86 @@
-import React from "react";
+import React, { useState } from "react";
 import "../css/game.css";
 
+import { checkValidMove } from "../logic/GameBoard_logic";
+
+let Board = [
+  [0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 1, 2, 0, 0, 0],
+  [0, 0, 0, 2, 1, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0],
+];
+
 const GameBoard = () => {
-  const onClickEvent = (e) => {
-    // 1度置かれた場所は、置けなくする。
-    if (e.target.childElementCount > 0) {
+  const [board, setBoard] = useState(Board);
+  const [player, setPlayer] = useState(1);
+
+  // GPT
+  const handleClick = (e) => {
+    const row = Number(e.target.getAttribute("data-row"));
+    const col = Number(e.target.getAttribute("data-column"));
+
+    if (board[row][col] !== 0) {
       return;
     }
-    const newElement = document.createElement("div");
-    newElement.setAttribute("class", "koma");
-    e.target.appendChild(newElement);
+
+    if (e.target.children.length === 0) {
+      alert("ココには置けないよ～");
+      return;
+    }
+
+    // クリックしたセルに自分の石を置く
+    const newBoard = [...board];
+    newBoard[row][col] = player;
+    setBoard(newBoard);
+
+    // 相手の石を挟む処理
+    checkValidMove(newBoard, row, col, player, true);
+
+    // プレイヤーを切り替える
+    setPlayer(player === 1 ? 2 : 1);
+  };
+
+  const Sample = (value) => {
+    switch (value) {
+      case 1:
+        return <div className="koma-kuro"></div>;
+      case 2:
+        return <div className="koma-shiro"></div>;
+
+      default:
+        break;
+    }
   };
 
   return (
     <>
-      <div className="board">
-        {[...Array(8)].map((_, rowIndex) => {
-          return (
-            <div key={`row${rowIndex}`} className="row">
-              {[...Array(8)].map((_, columnIndex) => {
-                if (
-                  (rowIndex === 3 && columnIndex === 3) ||
-                  (rowIndex === 4 && columnIndex === 4)
-                ) {
+      <div className="board-wrap">
+        <div className="board">
+          {[...Array(8)].map((_, rowIndex) => {
+            return (
+              <div key={`row${rowIndex}`} className="row">
+                {[...Array(8)].map((_, columnIndex) => {
                   return (
                     <div
                       key={`${rowIndex}${columnIndex}`}
                       className="box"
-                      onClick={onClickEvent}
+                      data-row={rowIndex}
+                      data-column={columnIndex}
+                      onClick={handleClick}
                     >
-                      <div className="koma-shiro"></div>
+                      {Sample(board[rowIndex][columnIndex])}
+                      {checkValidMove(board, rowIndex, columnIndex, player)}
                     </div>
                   );
-                } else if (
-                  (rowIndex === 3 && columnIndex === 4) ||
-                  (rowIndex === 4 && columnIndex === 3)
-                ) {
-                  return (
-                    <div
-                      key={`${rowIndex}${columnIndex}`}
-                      className="box"
-                      onClick={onClickEvent}
-                    >
-                      <div className="koma-kuro"></div>
-                    </div>
-                  );
-                } else {
-                  return (
-                    <div
-                      key={`${rowIndex}${columnIndex}`}
-                      className="box"
-                      onClick={onClickEvent}
-                    ></div>
-                  );
-                }
-              })}
-            </div>
-          );
-        })}
+                })}
+              </div>
+            );
+          })}
+        </div>
       </div>
     </>
   );
---
+++ src/logic/GameBoard_logic.jsx
@@ -0,0 +1,56 @@
+import React from "react";
+
+export const checkValidMove = (board, row, col, player, event = false) => {
+  const directions = [
+    { row: -1, col: 0 }, // 上
+    { row: 1, col: 0 }, // 下
+    { row: 0, col: -1 }, // 左
+    { row: 0, col: 1 }, // 右
+    { row: -1, col: -1 }, // 左上
+    { row: -1, col: 1 }, // 右上
+    { row: 1, col: -1 }, // 左下
+    { row: 1, col: 1 }, // 右下
+  ];
+
+  for (const direction of directions) {
+    const { row: dirRow, col: dirCol } = direction;
+    let currentRow = row + dirRow;
+    let currentCol = col + dirCol;
+    let flipCells = [];
+
+    // 相手の石が続く限り探索
+    while (
+      currentRow >= 0 &&
+      currentRow < board.length &&
+      currentCol >= 0 &&
+      currentCol < board[0].length &&
+      board[currentRow][currentCol] === (player === 1 ? 2 : 1)
+    ) {
+      flipCells.push({ row: currentRow, col: currentCol });
+      currentRow += dirRow;
+      currentCol += dirCol;
+    }
+
+    // 相手の石が続いていた場合、その方向に石を挟む
+    if (
+      flipCells.length > 0 &&
+      currentRow >= 0 &&
+      currentRow < board.length &&
+      currentCol >= 0 &&
+      currentCol < board[0].length &&
+      board[currentRow][currentCol] === player
+    ) {
+      if (event === true) {
+        for (const flipCell of flipCells) {
+          board[flipCell.row][flipCell.col] = player;
+        }
+      } else {
+        if (board[row][col] > 0) {
+          return null;
+        } else {
+          return <div className="placed"></div>;
+        }
+      }
+    }
+  }
+};
